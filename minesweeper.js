1053d6a870740245c79e94760f7b4759db34d57b
wrkbr <zcarc1@gmail.com>
2019-08-23 05:14:01 +0900

    8-2. 지뢰 심기

--- minesweeper.js
+++ minesweeper.js
@@ -9,6 +9,7 @@ document.querySelector('#execute').addEventListener('click', function(){
 
     console.log(horizontal, vertical, mine);
 
+    // Create mine table.
     var tbody = document.querySelector('#table tbody');
     var dataset = [];
     for(var i = 0; i < horizontal; i += 1) {
@@ -29,4 +30,48 @@ document.querySelector('#execute').addEventListener('click', function(){
 
     console.log(dataset);
 
+
+
+
+    // Draw out mine location.
+    var candidate = Array(horizontal * vertical).fill().map(function (Element, Index) {
+        return Index;
+    })
+    
+    console.log(candidate);
+    console.log(candidate.length);
+    
+    // 피셔 예이츠 셔플 이라는 알고리즘이다.
+    var fisherYatesShuffle = [];
+    
+    while(candidate.length > 80) {
+        var math = Math.floor( Math.random() * candidate.length );
+        console.log('math: ', math);
+        var value = candidate.splice( math , 1 )[0];
+        fisherYatesShuffle.push(value);
+    }
+    
+    console.log(fisherYatesShuffle);
+    
+
+
+ 
+    // Land mine.
+    for(var k = 0; k < fisherYatesShuffle.length; k++) {
+
+        var horizontalLocation =  Math.floor(fisherYatesShuffle[k] / 10);
+        var verticalLocation = fisherYatesShuffle[k] % 10;
+
+        console.log('fisherYatesShuffle[k]: ', fisherYatesShuffle[k]);
+        console.log('fisherYatesShuffle[k] / 10: ', fisherYatesShuffle[k] / 10);
+        console.log('Math.floor(fisherYatesShuffle[k] / 10): ', horizontalLocation);
+        console.log('fisherYatesShuffle % 10: ', verticalLocation);
+        tbody.children[horizontalLocation].children[verticalLocation].textContent = 'X';
+        dataset[horizontalLocation][verticalLocation] = 'X';
+    }
+
+    console.log('after Land mine dataset: ', dataset);
+
 });
+
+
